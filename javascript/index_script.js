7000750675694cdc29113422d596ec384a369dff
Agustín Ruatta <agustinruatta@gmail.com>
2022-05-11 22:08:44 -0300

    Mostrar productos en pantalla principal

--- javascript/index_script.js
+++ javascript/index_script.js
@@ -5,3 +5,29 @@ fetch('https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/sta
 
         document.getElementById('cantidad-productos').appendChild(textNode)
     })
+
+
+fetch('https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products')
+    .then(response => response.json())
+    .then(json => {
+        for(let producto of json) {
+            let imagen = document.createElement('img');
+            imagen.src = producto.image_url;
+            imagen.className = 'product-image';
+            imagen.alt = 'Imagen de notebook';
+
+            let nombre = document.createElement('p');
+            nombre.appendChild(document.createTextNode(producto.title));
+
+            let descripcion = document.createElement('p');
+            descripcion.appendChild(document.createTextNode(producto.price));
+
+            let div = document.createElement('div');
+            div.appendChild(imagen);
+            div.appendChild(nombre);
+            div.appendChild(descripcion);
+            div.className = 'product'
+
+            document.getElementById('product-list').appendChild(div);
+        }
+    })
