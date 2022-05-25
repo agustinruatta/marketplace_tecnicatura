be4c35ee1aa4b1a295eb1d485e47fb52fd484063
Agustín Ruatta <agustinruatta@gmail.com>
2022-05-24 21:43:17 -0300

    Agregado posibilidad de comprar

--- javascript/product_script.js
+++ javascript/product_script.js
@@ -6,7 +6,13 @@ Vue.createApp({
             description: "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
             image_url: "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
             factory_url: "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
-            price: "$ 98.038"
+            price: "$ 98.038",
+            cantidadProductosComprados: 0
+        }
+    },
+    methods: {
+        comprarProducto() {
+            this.cantidadProductosComprados++;
         }
     }
 }).mount('#app')
