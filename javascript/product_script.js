d06de4bb3ba7281bf8b3dcbc04700d15808269fc
Agustín Ruatta <agustinruatta@gmail.com>
2022-05-24 22:10:07 -0300

    Mejorado el carro de compra

--- javascript/product_script.js
+++ javascript/product_script.js
@@ -7,26 +7,34 @@ Vue.createApp({
             imageUrl: "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
             factoryUrl: "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
             price: 100000,
-            urlImagenes: [
+            imagesURL: [
                 "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
                 "https://bangho.vtexassets.com/arquivos/ids/160155/Notebook-Max-L5-i5-cuotas-sin-interes.jpg?v=637697603076200000"
             ],
-            variedadesNotebooks: [
-                {cantidadRam: '8 GB',  precio: 100000},
-                {cantidadRam: '16 GB',  precio: 125000},
+            notebooksTypes: [
+                {ramAmount: '8 GB',  price: 100000},
+                {ramAmount: '16 GB',  price: 125000},
             ],
-            cantidadProductosComprados: 0
+            boughtNotebooks: [],
         }
     },
     methods: {
-        comprarProducto() {
-            this.cantidadProductosComprados++;
+        buyNotebook() {
+            this.boughtNotebooks.push({title: this.title, price: this.price});
         },
-        cambiarImagen(urlImagen) {
-            this.imageUrl = urlImagen;
+        changeImage(imageUrl) {
+            this.imageUrl = imageUrl;
         },
-        cambiarPrecio(precio) {
-            this.price = precio;
+        changePrice(price) {
+            this.price = price;
+        },
+        totalAmount() {
+            let total = 0;
+            for(let boughtNotebook of this.boughtNotebooks) {
+                total+= boughtNotebook.price;
+            }
+
+            return total;
         }
     }
 }).mount('#app')
